73cda86185740b739625cbdc14775a84617dd210
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2016-03-25 15:45:54 +0000

    Added functions to the routes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Repositories\CategoryRepository;
+use Illuminate\Support\Facades\Response;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Show all the categories, just `id` and `name`
+     *
+     * @param CategoryRepository $categoryRepository
+     * @return mixed
+     */
+    public function index(CategoryRepository $categoryRepository)
+    {
+        return Response::json($categoryRepository->all(['id', 'name']));
+    }
+
+    /**
+     * Show all the details from one category, with the total of products in that category
+     *
+     * @param int $categoryId
+     * @param CategoryRepository $categoryRepository
+     * @return mixed
+     */
+    public function show($categoryId, CategoryRepository $categoryRepository)
+    {
+        $category = $categoryRepository->find($categoryId);
+        $category->totalProducts();
+
+        return Response::json($category);
+    }
 }
