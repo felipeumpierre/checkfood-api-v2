cb05f0830b9bd81046a3bae6c25e02eb52bda6bd
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2016-03-27 23:52:50 +0000

    Removed unnecessary index of the return array

--- app/Http/Controllers/OpinionController.php
+++ app/Http/Controllers/OpinionController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Repositories\OpinionRepository;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class OpinionController extends Controller
 {
-    //
+    /**
+     * Show all the opinions
+     *
+     * @param OpinionRepository $opinionRepository
+     * @return string
+     */
+    public function index(OpinionRepository $opinionRepository)
+    {
+        return Response::json($opinionRepository->all());
+    }
+
+    /**
+     * Insert a new opinion
+     *
+     * @param Request $request
+     * @param OpinionRepository $opinionRepository
+     * @return string
+     */
+    public function create(Request $request, OpinionRepository $opinionRepository)
+    {
+        try {
+            $response = $opinionRepository->create($request->all());
+        } catch (\Exception $e) {
+            $response = [
+                'message' => 'An error happened.',
+                'error' => 'UNEXPECTED_ERROR',
+            ];
+        }
+
+        return Response::json($response);
+    }
 }
