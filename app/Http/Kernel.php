fd31689307e91903b6a6b63b10da335faa54d547
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2016-04-10 12:47:29 +0000

    Added middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,5 +48,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'api.product' => \App\Http\Middleware\ProductMiddleware::class,
     ];
 }
