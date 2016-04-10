a9857ea1a337559a1c4c4e824f1ce57bed5d25c3
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2016-04-10 12:50:24 +0000

    Product Trait

--- app/Services/Traits/ProductTrait.php
+++ app/Services/Traits/ProductTrait.php
@@ -1,15 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: felipeumpierre
- * Date: 10/04/16
- * Time: 13:30
- */
 
 namespace App\Services\Traits;
 
+use Dingo\Api\Http\Response\Factory;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+use App\Repositories\ProductRepository;
 
 trait ProductTrait
 {
+    /**
+     * Check if the product exists and create a cache of the result
+     *
+     * @param  integer $id
+     * @param  ProductRepository $productRepository
+     * @param  Factory $factory
+     */
+    public function productExists($id, ProductRepository $productRepository, Factory $factory)
+    {
+        $exists = Cache::remember(sprintf('product.exists.%d', $id), Config::get('checkfood.cache.main'), function () use ($id, $productRepository) {
+            return $productRepository->exists($id);
+        });
 
+        if (!$exists) {
+            $factory->errorNotFound('Product not found');
+        }
+    }
 }
