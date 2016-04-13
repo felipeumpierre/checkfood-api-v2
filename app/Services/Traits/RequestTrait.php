899d0aae4f05561c5c8ce8705b1512ce20a3f3a3
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2016-04-13 22:09:46 +0000

    Request Traits

--- app/Services/Traits/RequestTrait.php
+++ app/Services/Traits/RequestTrait.php
@@ -2,18 +2,51 @@
 
 namespace App\Services\Traits;
 
-use App\Repositories\RequestRepository;
+use App\Repositories\ProductRepository;
+use App\Repositories\RequestObservationRepository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-trait Request
+trait RequestTrait
 {
-    public function addProducts(Request $request, RequestRepository $requestRepository)
+    /**
+     * @param  Request $request
+     * @param  Model $model
+     * @param  ProductRepository $productRepository
+     */
+    public function addProducts(Request $request, Model $model, ProductRepository $productRepository)
     {
+        foreach ($request->get('products') as $index => $product) {
+            $productObject = $productRepository->find($product['id']);
 
+            $model->products()->attach(
+                $productObject,
+                [
+                    'unity_price' => $productObject->price,
+                    'quantity' => $product['quantity'],
+                    'total_price' => ($product['quantity'] * $productObject->price),
+                ]
+            );
+        }
+
+        $this->addObservation($request, $model);
     }
 
-    public function addObservation()
+    /**
+     * @param  Request $request
+     * @param  Model $model
+     */
+    public function addObservation(Request $request, Model $model)
     {
+        foreach ($request->get('products') as $index => $product) {
+            if (empty($product['observation']) || is_null($product['observation'])) {
+                continue;
+            }
 
+            $requestObservationRepository = (new RequestObservationRepository(app()))->getCurrentPlan()->getModel();
+            $requestObservationRepository->requests_products_id = $model->id;
+            $requestObservationRepository->observation = $product['observation'];
+            $requestObservationRepository->save();
+        }
     }
 }
